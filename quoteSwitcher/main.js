790857ca83228d157c18731cca0a41e6e588c9ce
Sven Huff <info@sven-huff.de>
2021-08-12 17:07:36 +0200

    Add data-attributes

--- quoteSwitcher/main.js
+++ quoteSwitcher/main.js
@@ -1,7 +1,21 @@
-var circle = document.querySelectorAll(".image-class");
+let circles = document.querySelectorAll(".image-class");
+let quote = document.getElementById("display-quote");
 
-circle.forEach((e) => {
-  e.addEventListener("click", (e) => {
-    console.log(e);
+// TODO Ternary Operator
+quotator = (character) => {
+  if (character == "spiderman") {
+    quote.textContent = "Hi, I am Spiderman";
+  } else if (character == "mrkrabs") {
+    quote.textContent = "Hi, I am Mr. Krabs";
+  } else {
+    quote.textContent = "Hi, I am Spongebob";
+  }
+};
+
+circles.forEach((circle) => {
+  circle.addEventListener("click", () => {
+    let character = circle.getAttribute("data-character");
+    circle.classList.toggle("active");
+    quotator(character);
   });
 });

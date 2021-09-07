723a35c307d1ebdad33414a304b984914262b9e5
Sven Huff <info@sven-huff.de>
2021-09-07 17:20:37 +0200

    Add rainbow

--- the_odin_project/etch/main.js
+++ the_odin_project/etch/main.js
@@ -1,4 +1,10 @@
-// declaring variable outside makes it available
+let black = document.getElementById("black");
+let rainbow = document.getElementById("rainbow");
+let eraser = document.getElementById("delete");
+
+let eraserFunc;
+let rainbowColor;
+
 let div;
 let container = document.getElementsByClassName("grid");
 for (let index = 1; index <= 32; index++) {
@@ -8,6 +14,32 @@ for (let index = 1; index <= 32; index++) {
   }
 }
 
+rainbow.addEventListener("click", () => {
+  rainbowColor = true;
+});
+
+eraser.addEventListener("click", () => {
+  eraserFunc = true;
+});
+
+// Muss checken, ob innerhalb der Funktion "eraser" geklickt wird.
+// => Listen constantly to events
 container[0].addEventListener("mouseover", (e) => {
-  e.target.classList.toggle("black");
+  if (eraserFunc == true) {
+    e.target.style.backgroundColor = "red";
+  }
+  if (rainbowColor == true) {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let a = Math.random().toFixed(1);
+    console.log(r, g, b, a);
+    e.target.style.backgroundColor =
+      "rgba(" + r + "," + g + "," + b + "," + a + ")";
+  }
+  if (e.target.classList != "black") {
+    e.target.classList = "black";
+  } else {
+    e.target.classList = "black";
+  }
 });
